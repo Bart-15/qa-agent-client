1af13b077cf8f8d15a7d4ce20f561211aafeb78b
Bart-15 <brat.tabusao@gmail.com>
2025-09-05 21:09:09 +0800

    refactor: ♻️  move logic inside useGetChatHistory

--- src/features/chat/index.tsx
+++ src/features/chat/index.tsx
@@ -1,26 +1,11 @@
-import { useEffect } from 'react';
-
 import { ChatContainer } from './components/chat-container';
 import { useGetChatHistory } from './hooks/useGetChatHistory';
-import { useChatStore } from './store/useChat.store';
 
 const Chat = () => {
-  const chatStore = useChatStore((state) => state);
-  const { data: chats } = useGetChatHistory();
-
-  useEffect(() => {
-    if (chats?.chatHistory) {
-      // Initialize chat session first if not exists
-      if (!chatStore.chat) {
-        chatStore.startChat(chats.sessionId);
-      }
-      chatStore.setChatHistory(chats.chatHistory);
-    }
-  }, [chats]);
+  useGetChatHistory();
 
   const handleSendMessage = async (message: string) => {};
 
-  console.log(chatStore.chat?.chatHistory);
   return <ChatContainer isLoading={false} onSendMessage={handleSendMessage} />;
 };
 
